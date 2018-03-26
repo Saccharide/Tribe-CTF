4926c756919b219b35103b0c9945f6104e1562ce
Saccharide <rtang@email.wm.edu>
2018-03-26 09:16:53 -0700

    removed some files

--- challenge1/challenge_1.py
+++ challenge1/challenge_1.py
@@ -5,10 +5,6 @@
 '''
 ##################################################################################################
 
-
+# Main Program
 # generate the ASCII character of the key to simplecrackme
-s = "\x54\x30\x24\x40\x66\xC5\x2F\x3A\x57\x7B"
-f = open('o', 'w')
-f.write(s)
-f.close()
-
+print "\x54\x30\x24\x40\x66\xC5\x2F\x3A\x57\x7B"
