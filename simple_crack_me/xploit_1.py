2567ae1b94d5c64bf56bac6c99e40bf463f4ec54
Saccharide <rtang@email.wm.edu>
2018-03-31 12:59:02 -0700

    renamed it to a better one :D

---
+++ simple_crack_me/xploit_1.py
@@ -0,0 +1,10 @@
+##################################################################################################
+'''
+// @Project      ManTech CTF 2018 Challenge 1
+// @Author       Saccharide
+'''
+##################################################################################################
+
+# Main Program
+# generate the ASCII character of the key to simplecrackme
+print "\x54\x30\x24\x40\x66\xC5\x2F\x3A\x57\x7B"
